3731e7e0e5dc9ec184d29bde04a2f125401190b0
LandonP172 <141693593+LandonP172@users.noreply.github.com>
2024-01-31 20:49:10 -0700

    changed some paths and fixed some errors, not even close it seems

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
+const path = require('path');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -17,11 +19,11 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 // Start Apollo Server with the GraphQL schema
