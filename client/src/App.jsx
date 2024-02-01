39495ee47a7fdbcd5616d30a941803f2475f3111
LandonP172 <141693593+LandonP172@users.noreply.github.com>
2024-01-31 17:59:58 -0700

    great now its 1/4 of the way working but no css for some god forsaken reason

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,15 @@
-import React from 'react';
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
+import './App.css';
+import { Outlet } from 'react-router-dom';
 
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
+import Navbar from './components/Navbar';
 
-const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
-});
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+}
 
-const Root = () => (
-  <ApolloProvider client={client}>
-    <Router>
-      <App />
-    </Router>
-  </ApolloProvider>
-);
-
-export default Root;
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,28 +1,22 @@
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import App from './App.jsx'
-import SearchBooks from './pages/SearchBooks'
-import SavedBooks from './pages/SavedBooks'
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <h1 className='display-2'>Wrong page!</h1>,
-    children: [
-      {
-        index: true,
-        element: <SearchBooks />
-      }, {
-        path: '/saved',
-        element: <SavedBooks />
-      }
-    ]
-  }
-])
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
-)
+  <ApolloProvider client={client}>
+    <Router>
+      <App />
+    </Router>
+  </ApolloProvider>
+);
